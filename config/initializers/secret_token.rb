24857a0e0a34e0f2375ace6bfb4497a968ff7955
freetrader <phil.balderson@mac.com>
2012-05-14 20:36:19 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gendec::Application.config.secret_token = 'f4a6a7170a540e5c43efb880c78d70d2efab3a5069f7d830c435efbb1b512d50a6b9ae82811b00cb81d83ba520a346937e93fa00873c863bb5a7ee6a897a96f0'
